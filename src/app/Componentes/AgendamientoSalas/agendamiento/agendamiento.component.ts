49d29873ec2b087289e8e420cb18a5553c4ab3ce
2000Julian <jd.sanchez.2022.1@gmail.com>
2025-02-26 23:57:11 -0500

    se ajusta vista de modulos

--- src/app/Componentes/AgendamientoSalas/agendamiento/agendamiento.component.ts
+++ src/app/Componentes/AgendamientoSalas/agendamiento/agendamiento.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarioComponent } from '../../calendario/calendario.component';
+import { RouterModule } from '@angular/router';
+import { ListarSalasComponent } from '../../Salas/listar-salas/listar-salas.component';
 
 @Component({
   selector: 'app-agendamiento',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalendarioComponent,RouterModule, ListarSalasComponent],
   templateUrl: './agendamiento.component.html',
   styleUrls: ['./agendamiento.component.css']
 })
-export class AgendamientoComponent { }
+export class AgendamientoComponent { 
+
+  vistaActual: string = 'calendario'; // Por defecto, muestra el calendario
+
+  cambiarVista(vista: string) {
+    this.vistaActual = vista;
+  }
+  
+}
